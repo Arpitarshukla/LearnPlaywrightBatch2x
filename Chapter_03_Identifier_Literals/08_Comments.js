24755ba5ff763580f7d1e4a70089dcf978fc5089
Arpitarshukla <Arpitarshukla@users.noreply.github.com>
2026-05-18 23:08:46 +0530

    Add 08_Comments.js

---
+++ Chapter_03_Identifier_Literals/08_Comments.js
@@ -0,0 +1,8 @@
+//This is single comment and will be ignored by the JavaScript engine
+/* This is a multi-line comment
+   and it will also be ignored by the JavaScript engine */          
+/** This is a JSDoc comment 
+ * 
+*/
+
+//var g = 10;
