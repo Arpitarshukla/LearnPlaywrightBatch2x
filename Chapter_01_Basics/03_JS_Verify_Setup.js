3500f734a3b307f9dab758db3a2755f0419af09b
Arpitarshukla <Arpitarshukla@users.noreply.github.com>
2026-05-18 11:58:16 +0530

    Initial commit: Add Chapter_01_Basics files

---
+++ Chapter_01_Basics/03_JS_Verify_Setup.js
@@ -0,0 +1,11 @@
+console.log(process.platform);
+
+//MAC= DARWIN
+//WINDOWS= WIN32   
+//LINUX= LINUX
+
+console.log(process.arch);
+//X64= 64 BIT
+//arm64= 64 BIT
+
+console.log("node.version:", process.version);
