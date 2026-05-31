db1c06bfcec898bd2597fb8bb8ec8bae535b8997
Arpitarshukla <Arpitarshukla@users.noreply.github.com>
2026-05-31 21:47:57 +0530

    Update README: add missing Chapter 11 array files, fix function expression example

--- Chapter_12_Functions/102_Fn_Expression.js
+++ Chapter_12_Functions/102_Fn_Expression.js
@@ -11,17 +11,18 @@ console.log(r);
 
 //we can use let also but mostly const is used as we mostly want the function name will not change
 
-//Normal type 1 function  can also be written like line 19
+//Type 4 - Normal function      //can also be written like line 19
 function greet1(name1) {
     return `Hello,${name1}!`;
 }
+greet1("Pramod");
 
 //Function as Expression------->Normal type 1 function now as expression
 
-const greet1(name1){
+const greet2 = function (name1) {
     return `Hello,${name1}!`;
 }
-
+greet2("Bob");
 
 
 
